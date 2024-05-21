e98217bf2cd7dcee0f7062e3ce9ebc0d30859343
rtoledo_dev <rodrigo@rtoledo.inf.br>
2024-05-21 15:25:53 -0300

    Update project settings for EventsControl app

--- ios/HostelTribesTests/HostelTribesTests.m
+++ ios/HostelTribesTests/HostelTribesTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface HostelTribesTests : XCTestCase
+@interface EventsControlTests : XCTestCase
 
 @end
 
-@implementation HostelTribesTests
+@implementation EventsControlTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
